6b54b61762a732c0bdfaa3653d8de774b93c8e12
Salim Qadri <sqadri@rics.bwh.harvard.edu>
2022-11-29 15:18:36 -0500

    Netlify deploy for SSR and vite .env

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,14 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 import preact from "@astrojs/preact";
-
 import image from "@astrojs/image";
+
+// https://astro.build/config
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [preact(), image()]
+  integrations: [preact(), image()],
+  adapter: netlify()
 });
